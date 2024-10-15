5c5999ef3f31e5500fd560e395c041090f979480
Sergey Pokusaev <work.gooner1886@gmail.com>
2024-10-15 21:16:37 +0400

    подключил реакт, настройка css

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,14 +1,15 @@
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: ["style-loader", "css-loader", "sass-loader"],
+  };
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
 
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
-
-    return [
-        typescriptLoader,
-    ]
+  return [typescriptLoader, cssLoader];
 }
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export function someFn(arg: number): string {
-    console.log('RANDOM FUNCTION 12312312312')
-
-    return ''
-}
