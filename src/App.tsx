2ef9838d177800f2315101da10a00adfc8baded6
Sergey Pokusaev <work.gooner1886@gmail.com>
2024-10-21 21:15:07 +0400

    Добавление роутинга и lazy_loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Counter } from "./components/Counter";
 import "./index.scss";
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 const App = () => {
   return (
     <div className="app">
-      <p>zxczxczxc</p>
-      <Counter />
+      <Link to={"/"}>Main</Link>
+      <Link to={"/about"}>About</Link>
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+          {/* <Counter /> */}
+        </Routes>
+      </Suspense>
     </div>
   );
 };
