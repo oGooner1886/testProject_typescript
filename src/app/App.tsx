c8027314b03f336e33cd0097f37bfcb2895f0290
Sergey Pokusaev <work.gooner1886@gmail.com>
2024-12-03 22:44:18 +0400

    добавление navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,16 @@
-import React from "react";
 import "./styles/index.scss";
-import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "./providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
+      <Navbar />
+      <AppRouter />
       <button onClick={toggleTheme}>тык</button>
-      <Link to={"/"}>Main</Link>
-      <Link to={"/about"}>About</Link>
-      <AppRouter/>
     </div>
   );
 };
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,12 +2,12 @@ type Mods = Record<string, boolean | string>;
 
 export function classNames(
   classNames: string,
-  mods: Mods,
-  additional: string[]
+  mods: Mods = {},
+  additional: string[] = []
 ): string {
   return [
     classNames,
-    ...additional,
+    ...additional.filter(Boolean),
     Object.entries(mods)
       .filter(([classNames, value]) => Boolean(value))
       .map(([classNames]) => classNames),
