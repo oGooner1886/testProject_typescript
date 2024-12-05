18e2b10f4ef022f9a1e126d47d7564106e626711
Sergey Pokusaev <work.gooner1886@gmail.com>
2024-12-05 21:59:56 +0400

    Добавил сайдбар

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -7,7 +7,15 @@ const AppRouter = () => {
     <Suspense fallback={<div>Загрузка...</div>}>
       <Routes>
         {Object.values(routeConfig).map(({ path, element }) => (
-          <Route key={path} path={path} element={element} />
+          <Route
+            key={path}
+            path={path}
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <div className="page-wrapper">{element}</div>
+              </Suspense>
+            }
+          />
         ))}
       </Routes>
     </Suspense>
