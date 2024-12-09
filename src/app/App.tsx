15c47d00be07bf515adaaddc1f4322e4966592fc
Sergey Pokusaev <work.gooner1886@gmail.com>
2024-12-09 22:35:57 +0400

    фикс страниц, добавление словарика для перевода, добавление логики на кнопку

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,21 +4,8 @@ import { useTheme } from "./providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
-import { useTranslation } from "react-i18next";
 import { Suspense } from "react";
-
-const ChangeLang = () => {
-  const { t, i18n } = useTranslation();
-  const toggleLang = () => {
-    i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
-  };
-  return (
-    <div>
-      <button onClick={toggleLang}>{t("Перевести")}</button>
-      {t("Тестовый пример")}
-    </div>
-  );
-};
+import { LangSwitcher } from "shared/ui/LangSwitcher";
 
 const App = () => {
   const { theme } = useTheme();
@@ -26,7 +13,6 @@ const App = () => {
     <div className={classNames("app", {}, [theme])}>
       <Suspense fallback="">
         <Navbar />
-        <ChangeLang />
         <div className="content-page">
           <Sidebar />
           <AppRouter />
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Sidebar.module.scss";
 import { useState } from "react";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import { LangSwitcher } from "shared/ui/LangSwitcher";
 interface SidebarProps {
   className?: string;
 }
@@ -21,7 +22,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
       <button onClick={onToggle}>toggle</button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        {/* LanguageSwitcher */}
+        <LangSwitcher className={cls.lang} />
       </div>
     </div>
   );
