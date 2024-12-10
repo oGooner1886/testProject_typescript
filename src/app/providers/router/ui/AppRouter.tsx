c074d66716d00fc0922d860c353e8142c06b7e14
Sergey Pokusaev <work.gooner1886@gmail.com>
2024-12-10 23:16:35 +0400

    добавлен loader для страниц, валидация несуществующих маршрутов

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,24 +1,23 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { PageLoader } from "shared/ui/PageLoader/PageLoader";
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Загрузка...</div>}>
-      <Routes>
-        {Object.values(routeConfig).map(({ path, element }) => (
-          <Route
-            key={path}
-            path={path}
-            element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <div className="page-wrapper">{element}</div>
-              </Suspense>
-            }
-          />
-        ))}
-      </Routes>
-    </Suspense>
+    <Routes>
+      {Object.values(routeConfig).map(({ path, element }) => (
+        <Route
+          key={path}
+          path={path}
+          element={
+            <Suspense fallback={<PageLoader />}>
+              <div className="page-wrapper">{element}</div>
+            </Suspense>
+          }
+        />
+      ))}
+    </Routes>
   );
 };
 
