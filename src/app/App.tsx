db2b706da4951a921d32d89f6012d39d8871f765
Sergey Pokusaev <work.gooner1886@gmail.com>
2024-12-11 22:13:29 +0400

    добавлен error boundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,11 +4,12 @@ import { useTheme } from "./providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { LangSwitcher } from "shared/ui/LangSwitcher";
 
 const App = () => {
   const { theme } = useTheme();
+
   return (
     <div className={classNames("app", {}, [theme])}>
       <Suspense fallback="">
