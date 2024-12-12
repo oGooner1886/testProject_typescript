a38fe53775b61d4c7e5dac74455b8d513f768bd2
Sergey Pokusaev <work.gooner1886@gmail.com>
2024-12-12 20:28:24 +0400

    тест компоненты, настройка jest

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -15,26 +15,20 @@ export default {
 
   // Automatically clear mock calls, instances and results before every test
   clearMocks: true,
-  testEnvironment: 'jsdom',
-  coveragePathIgnorePatterns: [
-      '\\\\node_modules\\\\',
-  ],
-  moduleFileExtensions: [
-      'js',
-      'jsx',
-      'ts',
-      'tsx',
-      'json',
-      'node',
-  ],
-  moduleDirectories: [
-      'node_modules',
-  ],
+  testEnvironment: "jsdom",
+  coveragePathIgnorePatterns: ["\\\\node_modules\\\\"],
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+  moduleDirectories: ["node_modules"],
+  modulePaths: ["<rootDir>src"],
   testMatch: [
-      // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
-      '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
+    // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
+    "<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)",
   ],
-  rootDir: '../../',
+  rootDir: "../../",
+  setupFilesAfterEnv: ["<rootDir>config/jest/setupTests.ts"],
+  moduleNameMapper: {
+    "\\.s?css$": "identity-obj-proxy",
+  },
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
 
