133da5e262c0b5ea9ce857d3098208b5227379b3
Sergey Pokusaev <work.gooner1886@gmail.com>
2024-12-17 23:22:51 +0400

    fix toggle btn

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,12 +4,15 @@ import cls from "./Sidebar.module.scss";
 import { useState } from "react";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 import { LangSwitcher } from "shared/ui/LangSwitcher";
+import { Button } from "shared/ui/Button/Button";
+import { useTranslation } from "react-i18next";
 interface SidebarProps {
   className?: string;
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false);
+  const [t, i18n] = useTranslation();
   const onToggle = () => {
     setCollapsed((prev) => !prev);
   };
@@ -20,9 +23,9 @@ export const Sidebar = ({ className }: SidebarProps) => {
         className,
       ])}
     >
-      <button data-testid="sidebar-toggle" onClick={onToggle}>
-        toggle
-      </button>
+      <Button data-testid="sidebar-toggle" onClick={onToggle}>
+        {t("Свернуть")}
+      </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
